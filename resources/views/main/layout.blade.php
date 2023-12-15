dc3b6cb9d9136688a11543c4a746819fbd3d65e1
tavartkiladze94 <153375790+tavartkiladze94@users.noreply.github.com>
2023-12-15 16:52:05 +0400

    Update layout.blade.php

--- resources/views/main/layout.blade.php
+++ resources/views/main/layout.blade.php
@@ -229,7 +229,7 @@ function autocomplete(inp, arr) {
           b.addEventListener("click", function(e) {
               /*insert the value for the autocomplete text field:*/
               inp.value = this.getElementsByTagName("input")[0].value;
-              location.href = "{{asset('food_info')}}"+"/"+ this.getElementsByTagName("input")[1].value;
+              location.href = "https://allfoods.ishove.ge/food_info/"+ this.getElementsByTagName("input")[1].value;
               /*close the list of autocompleted values,
               (or any other open lists of autocompleted values:*/
               closeAllLists();
